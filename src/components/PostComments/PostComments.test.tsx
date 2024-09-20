4f6b25c09ef863e52cb1a0d0c1ef0799199db75e
Ana Camila Pantoja <anacamilapantoja26@gmail.com>
2024-09-20 16:44:14 -0300

    finalizando configurações

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,25 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react"
+import PostComment from '.'
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
-});
+    it('Renderizar o componente corretamente', () => {
+        render(<PostComment/>)
+        expect(screen.getByText('Comentar')).toBeInTheDocument()
+    })
+    test('Adicionar dois comentários e renderizá-los', () => {
+        render(<PostComment/>)
+        fireEvent.change(screen.getByTestId('textarea'), {
+            target: {
+                value: 'Primeiro comentário'
+            }
+        })
+        fireEvent.click(screen.getByTestId('button'))
+        fireEvent.change(screen.getByTestId('textarea'), {
+            target: {
+                value: 'Segundo comentário'
+            }
+        })
+        fireEvent.click(screen.getByTestId('button'))
+        expect(screen.getAllByTestId('comment')).toHaveLength(2)
+    })
+})
